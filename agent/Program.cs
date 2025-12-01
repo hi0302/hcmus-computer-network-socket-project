667489f22c12f7ac6f08f02f2568c21ffb225472
Meg-0911 <maiquynhanh1819@gmail.com>
2025-12-01 21:15:05 +0700

    Add files via upload

---
+++ agent/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using ClientAgent.Core; // Dòng này QUAN TRỌNG: để nhận diện ClientSocket
+
+// --- Điểm bắt đầu chương trình (Entry Point) ---
+
+// 1. Khởi tạo kết nối Socket chạy trên luồng nền
+ClientSocket.Initialize();
+
+// 2. Giữ cho ứng dụng chạy vĩnh viễn (không bị tắt ngay lập tức)
+// Vì là Agent chạy nền, ta cần treo tiến trình chính ở đây.
+Thread.Sleep(Timeout.Infinite);
