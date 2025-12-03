e7e670fb4d4560b1a59f81c1506fb62f0c9c580c
Diem Thuy <24120229@student.hcmus.edu.vn>
2025-12-03 13:17:57 +0700

    bổ sung powerhandler.cs

--- agent/Services/System/PowerHandler.cs
+++ agent/Services/System/PowerHandler.cs
@@ -1,12 +1,38 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.Versioning;
 
-namespace agent.Services.System
+namespace RemoteControl.Agent.Services.System
 {
-    internal class PowerHandler
+    [SupportedOSPlatform("windows")] // Chỉ chạy trên windows
+    public class PowerHandler
     {
+        // 1. Tắt máy tính
+        public bool Shutdown()
+        {
+            try
+            {
+                // Lệnh shutdown: /s = shutdown, /t 0 = ngay lập tức
+                Process.Start("shutdown", "/s /t 0");
+                return true;
+            }
+            catch { return false; }
+        }
+
+        // 2. Khởi động lại máy tính
+        public bool Restart()
+        {
+            try
+            {
+                // Lệnh restart: /r = restart, /t 0 = ngay lập tức
+                Process.Start("shutdown", "/r /t 0");
+                return true;
+            }
+            catch { return false; }
+        }
     }
 }
